805868106d70852ac60753419698b4cdf13e3db7
Vinicius Coutinho <vinicius@promofarma.com.br>
2025-01-22 18:08:40 -0300

    :feat Wip

--- config/cors.php
+++ config/cors.php
@@ -21,7 +21,7 @@ return [
 
     'allowed_methods' => ['GET', 'PATCH'],
 
-    'allowed_origins' => ['http://localhost', 'http://intranet.promofarma.int'],
+    'allowed_origins' => ['http://localhost', 'http://intranet.promofarma.int', 'https://intranet.promofarma.int'],
 
     'allowed_origins_patterns' => [],
 
