d9cf68ef724a1d6e01799a58fb17be242f7c72ea
Vinicius Coutinho <vinicius@promofarma.com.br>
2025-03-05 14:30:39 -0300

    :fix Add trusted proxies list

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -14,7 +14,9 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
-        //
+        $middleware->trustProxies([
+            '*',
+        ]);
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
